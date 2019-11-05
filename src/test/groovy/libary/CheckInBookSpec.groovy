f9419bd18d82fb294c0b5538dcd033a8c7d26f86
vathanak <vathanakkongsothybtb35@gmail.com>
2019-11-05 20:26:25 +0700

    add domain check in book

---
+++ src/test/groovy/libary/CheckInBookSpec.groovy
@@ -0,0 +1,18 @@
+package libary
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class CheckInBookSpec extends Specification implements DomainUnitTest<CheckInBook> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
